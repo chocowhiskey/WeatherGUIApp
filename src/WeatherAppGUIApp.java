ab3eb91eb99ec292d0024711135632fe65ff21a4
Simone <simohoff@gmx.de>
2025-04-06 12:03:12 +0200

    Add search text wip

--- src/WeatherAppGUIApp.java
+++ src/WeatherAppGUIApp.java
@@ -1,18 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class WeatherAppGUIApp extends JFrame {
-    public WeatherAppGUIApp () {
-        super( "Weather App");
+    public WeatherAppGUIApp() {
+        super("Weather App");
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        setSize(450,650);
+        setSize(450, 650);
 
         setLocationRelativeTo(null);
 
         setLayout(null);
 
         setResizable(false);
+
+        addGUIComponents();
     }
 
+    private void addGUIComponents() {
+        JTextField searchTextField = new JTextField();
+
+        searchTextField.setBounds(15, 15, 351, 45);
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
+    }
 }
