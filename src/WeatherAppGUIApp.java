f48823d223d1b32d35debe19cea1bead582593a5
Simone <simohoff@gmx.de>
2025-04-06 11:57:54 +0200

    Add frame wip

---
+++ src/WeatherAppGUIApp.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class WeatherAppGUIApp extends JFrame {
+    public WeatherAppGUIApp () {
+        super( "Weather App");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(450,650);
+
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+
+        setResizable(false);
+    }
+
+}
