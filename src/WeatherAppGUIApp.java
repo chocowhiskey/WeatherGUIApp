f8e24b6a4dd66a6e5b2139bea9b423d61fd857b7
Simone <simohoff@gmx.de>
2025-04-06 12:37:12 +0200

    Add windspeed and humidity part

--- src/WeatherAppGUIApp.java
+++ src/WeatherAppGUIApp.java
@@ -40,6 +40,42 @@ public class WeatherAppGUIApp extends JFrame {
         JLabel weatherConditionImage = new JLabel(loadImage("src/assets/clear.png"));
         weatherConditionImage.setBounds(0,125,450,217);
         add(weatherConditionImage);
+
+        // Add temperature text
+        JLabel temparatureText = new JLabel("20 C");
+        temparatureText.setBounds(0,350,450,54);
+        temparatureText.setFont(new Font("Dialog", Font.BOLD, 30));
+        temparatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temparatureText);
+
+        // Add description
+        JLabel weatherDesc = new JLabel("Sunny");
+        weatherDesc.setBounds(0,405,450,36);
+        weatherDesc.setFont(new Font("Dialog", Font.PLAIN, 20));
+        weatherDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherDesc);
+
+        // Add humidity image
+        JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+        humidityImage.setBounds(15,500,74,66);
+        add(humidityImage);
+
+        // Add humidity description
+        JLabel humidityDesc = new JLabel("<html><b>Humditiy level</b> 100%</html>");
+        humidityDesc.setBounds(90,500,85,55);
+        humidityDesc.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(humidityDesc);
+
+        // Add windspeed image
+        JLabel windspeedImg = new JLabel(loadImage("src/assets/windspeed.png"));
+        windspeedImg.setBounds(220,500,4,66);
+        add(windspeedImg);
+
+        // Add windspeed description
+        JLabel windspeedDesc = new JLabel("<html><b>Windspeed</b> 15km/h");
+        windspeedDesc.setBounds(310,500,85,55);
+        windspeedDesc.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(windspeedDesc);
     }
 
     private ImageIcon loadImage(String ressourcePath) {
