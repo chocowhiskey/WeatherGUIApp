a71dbbd76495f5ab9eb6257a0e777986fc62d413
Simone <simohoff@gmx.de>
2025-04-06 12:14:31 +0200

    Add search text wip

--- src/WeatherAppGUIApp.java
+++ src/WeatherAppGUIApp.java
@@ -1,5 +1,8 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class WeatherAppGUIApp extends JFrame {
     public WeatherAppGUIApp() {
@@ -19,11 +22,29 @@ public class WeatherAppGUIApp extends JFrame {
     }
 
     private void addGUIComponents() {
+        // Add text field
         JTextField searchTextField = new JTextField();
 
         searchTextField.setBounds(15, 15, 351, 45);
         searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         add(searchTextField);
+
+        // Add button for text field
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375,13,47,45);
+        add(searchButton);
+    }
+
+    private ImageIcon loadImage(String ressourcePath) {
+try {
+    BufferedImage image = ImageIO.read(new File(ressourcePath));
+    return new ImageIcon(image);
+} catch (Exception e) {
+    e.printStackTrace();
+}
+System.out.println("Could not find asset.");
+        return null;
     }
 }
